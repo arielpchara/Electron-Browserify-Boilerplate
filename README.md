70553216237a9f484379aa37fb616a76e6839221
Ariel Mac <ariel@pchara.com>
2015-10-05 20:37:42 -0300

    considate

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-# +1 Electron Boilerplate
+Electron Browserify Boilerplate
 - Gulp
 - Browserify
 - Angular 1
 - Nunjucks
 - Sass
-=======
-# widgets.pro 
->>>>>>> d5413e7f95b9405b259804a866277112349373cd
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,7 @@
 var gulp = require( 'gulp' );
 var requireDir = require( 'require-dir' );
-var runSequence = require('run-sequence');
 
 requireDir( './gulp' );
 
-
-gulp.task('watch',['compile'],function () {
-    gulp.watch(['src/**/*'], ["compile"]);
-});
 
 gulp.task('default',['compile']);
